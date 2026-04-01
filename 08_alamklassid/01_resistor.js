965f2336b2aaa49cd3fcc3275ce1d97c344ca81b
Buddha <161453197+Budbha@users.noreply.github.com>
2026-04-01 10:57:36 +0000

    ...................

---
+++ 08_alamklassid/01_resistor.js
@@ -0,0 +1,82 @@
+var __extends = (this && this.__extends) || (function () {
+    var extendStatics = function (d, b) {
+        extendStatics = Object.setPrototypeOf ||
+            ({ __proto__: [] } instanceof Array && function (d, b) { d.__proto__ = b; }) ||
+            function (d, b) { for (var p in b) if (Object.prototype.hasOwnProperty.call(b, p)) d[p] = b[p]; };
+        return extendStatics(d, b);
+    };
+    return function (d, b) {
+        if (typeof b !== "function" && b !== null)
+            throw new TypeError("Class extends value " + String(b) + " is not a constructor or null");
+        extendStatics(d, b);
+        function __() { this.constructor = d; }
+        d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
+    };
+})();
+// Абстрактный класс
+var AbstractResistor = /** @class */ (function () {
+    function AbstractResistor() {
+    }
+    AbstractResistor.prototype.getCurrent = function (u) {
+        return u / this.getResistance(); // I = U / R
+    };
+    return AbstractResistor;
+}());
+// Класс резистора
+var Resistor = /** @class */ (function (_super) {
+    __extends(Resistor, _super);
+    function Resistor(r) {
+        var _this = _super.call(this) || this;
+        _this.r = r;
+        return _this;
+    }
+    Resistor.prototype.getResistance = function () {
+        return this.r;
+    };
+    return Resistor;
+}(AbstractResistor));
+// Создание объекта
+var resistor1 = new Resistor(220);
+console.log("The resistance value of resistor 01: " + resistor1.getResistance());
+// Класс переключателя
+var Switch = /** @class */ (function (_super) {
+    __extends(Switch, _super);
+    function Switch() {
+        var _this = _super.call(this) || this; // сопротивление можно задать 0
+        _this.on = false;
+        return _this;
+    }
+    Switch.prototype.setOn = function (state) {
+        this.on = state;
+    };
+    //getResistance(): number {
+    //if(this.on){
+    //     return 0;
+    // }    
+    //else{
+    //     return 1000000000;
+    //  }
+    //}
+    Switch.prototype.getResistance = function () {
+        return (this.on ? 0 : 1000000000);
+    };
+    Switch.prototype.getCurrent = function (u) {
+        if (u > 0) {
+            if (this.on) {
+                throw new Error("Short Circuit");
+            }
+        }
+        return _super.prototype.getCurrent.call(this, u);
+    };
+    return Switch;
+}(AbstractResistor));
+var s1 = new Switch();
+console.log("The initial resistance valur it is off" + s1.getResistance());
+s1.setOn(true);
+console.log("The resistance value when the switch is on" + s1.getResistance());
+console.log(s1.getCurrent(5));
+//Current =u/resistance value
+//current=5/0=infinite
+s1.setOn(false);
+//current =5/1000000000= 5e-9
+console.log(s1.getCurrent(5));
---
+++ 08_alamklassid/01_resistor.ts
@@ -0,0 +1,76 @@
+// Абстрактный класс
+abstract class AbstractResistor {
+   
+
+    abstract getResistance(): number;
+
+    getCurrent(u:number):number{
+        return u / this.getResistance(); // I = U / R
+    }
+}
+
+// Класс резистора
+class Resistor extends AbstractResistor {
+
+    
+    r: number;
+    
+    constructor(r: number) {
+        super();
+        this.r=r;
+    }
+
+    getResistance(): number {
+        return this.r;
+    }
+}
+
+// Создание объекта
+let resistor1 = new Resistor(220);
+console.log("The resistance value of resistor 01: " + resistor1.getResistance());
+
+// Класс переключателя
+class Switch extends AbstractResistor {
+    on: boolean = false;
+
+    constructor() {
+        super(); // сопротивление можно задать 0
+    }
+
+    setOn(state: boolean) {
+        this.on = state;
+    }
+
+    //getResistance(): number {
+    //if(this.on){
+    //     return 0;
+    // }    
+    //else{
+    //     return 1000000000;
+    //  }
+    //}
+
+    getResistance(): number{
+        return(this.on ? 0:1000000000)
+    }
+  
+    getCurrent(u: number): number {
+        if (u>0){
+            if(this.on){
+                throw new Error("Short Circuit");
+            }
+        }
+        return super.getCurrent(u);
+    }
+}
+
+let s1=new Switch();
+console.log("The initial resistance valur it is off" + s1.getResistance());
+s1.setOn(true);
+console.log("The resistance value when the switch is on" + s1.getResistance());
+console.log(s1.getCurrent(5));
+//Current =u/resistance value
+//current=5/0=infinite
+s1.setOn(false);
+//current =5/1000000000= 5e-9
+console.log(s1.getCurrent(5));
