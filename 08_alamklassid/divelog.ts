b995a0ef782acef30c872e0c8bf253da34c8097d
Buddha <161453197+Budbha@users.noreply.github.com>
2026-05-23 14:53:24 +0000

    dz

--- 08_alamklassid/divelog.ts
+++ 08_alamklassid/divelog.ts
@@ -1,13 +1,9 @@
 class Diver {
-    name: string;
-    depth: number;
-    air: number;
-
-    constructor(name: string, depth: number, air: number) {
-        this.name = name;
-        this.depth = depth;
-        this.air = air;
-    }
+    constructor(
+        public name: string,
+        public depth: number,
+        public air: number
+    ) {}
 
     maxDepth(): number {
         return 18;
@@ -20,24 +16,72 @@ class Diver {
     isSafe(): boolean {
         return this.depth <= this.maxDepth() && this.hasEnoughAir();
     }
+
+    getType(): string {
+        return "Diver";
+    }
+
+    getInfo(): string {
+        return this.getType() +
+            " | " +
+            this.name +
+            " | depth: " + this.depth +
+            " m | air: " + this.air +
+            " | safe: " + this.isSafe();
+    }
 }
 
 class DeepDiver extends Diver {
     maxDepth(): number {
         return 40;
     }
+
+    getType(): string {
+        return "Deep diver";
+    }
 }
 
 class Instructor extends Diver {
     maxDepth(): number {
-        return 40; // можно оставить или больше
+        return 40;
+    }
+
+    getType(): string {
+        return "Instructor";
     }
 }
 
-let d1 = new Diver("Anna", 25, 300);
-let d2 = new DeepDiver("Mark", 35, 300);
-let d3 = new DeepDiver("Tom", 45, 300);
+let divers: Diver[] = [];
 
-console.log(d1.isSafe()); // false
-console.log(d2.isSafe()); // true
-console.log(d3.isSafe()); // false 
+function createDiver(): void {
+    const name = (document.getElementById("name") as HTMLInputElement).value;
+    const depth = (document.getElementById("depth") as HTMLInputElement).valueAsNumber;
+    const air = (document.getElementById("air") as HTMLInputElement).valueAsNumber;
+    const type = (document.getElementById("type") as HTMLSelectElement).value;
+
+    let diver: Diver;
+
+    if (type === "deep") {
+        diver = new DeepDiver(name, depth, air);
+    }
+    else if (type === "instructor") {
+        diver = new Instructor(name, depth, air);
+    }
+    else {
+        diver = new Diver(name, depth, air);
+    }
+
+    divers.push(diver);
+
+    updatePage();
+}
+
+function updatePage(): void {
+    let text = "";
+
+    for (const diver of divers) {
+        text += diver.getInfo() + "<br>";
+    }
+
+    document.getElementById("result")!.innerHTML = text;
+}
